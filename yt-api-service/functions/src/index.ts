10df051ab139fb7ce63ef7395c71f387c45a062e
Mark <markpanaro1@gmail.com>
2023-12-27 16:26:04 -0500

    Added method to fetch videos from Firestore

--- yt-api-service/functions/src/index.ts
+++ yt-api-service/functions/src/index.ts
@@ -12,6 +12,17 @@ const storage = new Storage();
 
 const rawVideoBucketName = "mbp99-yt-raw-videos";
 
+const videoCollectionId = "videos";
+
+export interface Video {
+  id?: string,
+  uid?: string,
+  filename?: string,
+  status?: "processing" | "processed",
+  title?: string,
+  description?: string
+}
+
 export const createUser = functions.auth.user().onCreate((user) => {
   const userInfo = {
     uid: user.uid,
@@ -48,4 +59,10 @@ export const generateUploadUrl = onCall({maxInstances: 1}, async (request) => {
   });
 
   return {url, fileName};
+});
+
+export const getVideos = onCall({maxInstances: 1}, async () => {
+  const snapshot =
+    await firestore.collection(videoCollectionId).limit(10).get();
+  return snapshot.docs.map((doc) => doc.data());
 });
