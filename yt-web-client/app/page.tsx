d9f332b7de97cc093550c11ae3bf75eb51b07cd1
Mark <markpanaro1@gmail.com>
2023-12-28 20:26:17 -0500

    Fixes and added web app docker image

--- yt-web-client/app/page.tsx
+++ yt-web-client/app/page.tsx
@@ -12,7 +12,7 @@ export default async function Home() {
     <main className={styles.main}>
       {
         videos.map((video) => (
-          <Link href={`/watch?v=${video.filename}`}>
+          <Link href={`/watch?v=${video.filename}`} key ={video.id}>
             <Image src={'/thumbnail.png'} alt='video' width={120} height={80}
               className={styles.thumbnail}/>
           </Link>
@@ -21,3 +21,5 @@ export default async function Home() {
     </main>
   )
 }
+
+export const revalidate = 30;
